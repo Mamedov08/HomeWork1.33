19da7b8c47859f3eddab58cc177929b1932edfcc
Mamedov08 <hanchikmamedov87@gmail.com>
2022-10-11 11:32:21 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework13/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.homework13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText To, Subject, Message;
+    Button Send;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        To = findViewById(R.id.et_too);
+        Message = findViewById(R.id.message);
+        Subject = findViewById(R.id.subject);
+        Send = findViewById(R.id.send);
+
+        Send.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String to = To.getText().toString();
+                String subject = Subject.getText().toString();
+                String message = Message.getText().toString();
+
+                Intent To = new Intent(Intent.ACTION_SEND);
+                To.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+                To.putExtra(Intent.EXTRA_SUBJECT, subject);
+                To.putExtra(Intent.EXTRA_TEXT, message);
+
+                To.setType("Email/XAH");
+
+            }
+        });
+    }
+}
